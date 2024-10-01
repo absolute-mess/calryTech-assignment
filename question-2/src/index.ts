afd43813b9426832b034a046fa8dfce7d2193a61
Sanskar Singh <singh822577@gmail.com>
2024-10-01 17:20:09 +0530

    Update index.ts

--- question-2/src/index.ts
+++ question-2/src/index.ts
@@ -13,26 +13,8 @@ class App {
 
     constructor() {
         this.app = express();
-        this.configureMiddleware();
         this.configureRoutes();
     }
-
-    private configureMiddleware() {
-        this.app.use(cors({ credentials: true }));
-        this.app.use(compression());
-        this.app.use(session({
-            secret: 'your_secret_key',
-            resave: false,
-            saveUninitialized: false,
-            cookie: {
-                secure: false,
-                maxAge: 24 * 60 * 60 * 1000
-            }
-        }));
-        this.app.use(cookieParser());
-        this.app.use(bodyParser.json());
-    }
-    
 
     private configureRoutes() {
         this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
